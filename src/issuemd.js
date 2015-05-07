a855c05eeb7eeacf05f33e5ff9896ac01a9a29d1
Billy Moon <billy@itaccess.org>
2015-05-07 23:26:22 +0100

    candidate

--- src/issuemd.js
+++ src/issuemd.js
@@ -3,84 +3,81 @@
 
     var parser = require('../dist/parser.js');
     var formatter = require('./issuemd-formatter.js');
-    var merge = require('./issuemd-merger.js');
+    var merger = require('./issuemd-merger.js');
     var utils = require("./utils.js");
 
     // serialize issue data object into issuemd style JSON
-    function toJSON () {
+    function toJSON (collection) {
         var ret = [];
-        for(var i=0; i<this.length; i++){
-            ret.push(this[i]);
+        for(var i=0; i<collection.length; i++){
+            ret.push(collection[i]);
         }
         return ret;
     }
 
     // when coerced into string, return issue collection as md
-    function toString (cols) {
-        return formatter.string(this.toArray(), cols);
+    function toString (collection, cols) {
+        return formatter.string(collection.toArray(), cols);
     }
 
     // return MD render of all ussues
     // TODO: accept md and merge into collection - perhaps call from main entry point
-    function md(input) {
+    function md(collection, input) {
         if(typeof input === "string"){
-            this.merge(input);
+            merger(collection, input);
         } else {
-            return formatter.md(this.toArray());
+            return formatter.md(collection.toArray());
         }
     }
 
     // return HTML render of all issues
-    function html() {
-        return formatter.html(this.toArray());
+    function html(collection) {
+        return formatter.html(collection.toArray());
     }
 
     // return a deep copy of a collection - breaking references
-    function clone(){
-        return issuemd(utils.copy(this.toArray()));
+    function clone(collection){
+        return issuemd(utils.copy(collection.toArray()));
     }
 
     // return new collection with *reference* to issue at `index` of original collection
-    function eq(index){
+    function eq(collection, index){
         var copy = issuemd(1);
-        copy[0] = this[index];
+        copy[0] = collection[index];
         return copy;
     }
 
-    function makeIssues(input){
+    function makeIssues(collection, input){
         for(var i = input; i--;){
-            this.push(utils.makeIssue());
+            collection.push(utils.makeIssue());
         }
-        return this;
+        return collection;
     }
 
     // loops over each issue - like underscore's each
-    function each(func){
-        utils.each(this, func);
-        return this;
+    function each(collection, func){
+        utils.each(collection, func);
+        return collection;
     }
 
     // remove the issues specified by `input` (accepts array, or one or more arguments specified indices to be deleted)
-    function remove(input){
+    function remove(collection, input){
 
         // set indices to input if it is an array, or arguments array (by converting from arguments array like object)
-        var indices = utils.typeof(input) === "array" ? input : Array.prototype.slice.call(arguments);
-
-        // get reference to current context (issue collection) for use in callback
-        var that = this;
+        var indices = utils.typeof(input) === "array" ? input : [].slice.call(arguments);
 
         // sort and reverse indices so that elements are removed from back, and don't change position of next one to remove
         indices.sort().reverse();
 
         utils.each(indices, function(index){
-            that.splice(index, 1);
+            collection.splice(index, 1);
         });
 
     }
 
 
     // modifies all issues with modified/modifier (defaults to now if null) and optional attrs object and/or comment string
-    function update(/* issue_update_object | modified, modifier[, (meta_array|meta_hash)][, body] */){
+    function update(collection /* issue_update_object | modified, modifier[, (meta_array|meta_hash)][, body] */){
         var args;
         if (arguments.length === 1) {
             args = arguments[0];
@@ -106,78 +103,82 @@
             }
         }
         // TODO: should default falsy modified value to `now`
-        this.each(function(issue){
+        collection.each(function(issue){
             issue.updates.push(args);
         });
-        return this;
+        return collection;
+    }
+
+    // creates or overwrites existing meta item with new key/val
+    function updateMeta (collection, obj){
+
+        collection.each(function(issue){
+            for(var key in obj){
+                var val = obj[key];
+                if(key === "title" || key === "created" || key === "creator" || key === "body"){
+                    issue.original[key] = val;
+                } else {
+                    var hit = false;
+                    // check all original meta values
+                    // TODO: refactor so we don't create function in loop
+                    utils.each(issue.original.meta, function(meta) {
+                        if(meta.key === key){
+                            meta.val = val;
+                            hit = true;
+                        }
+                    });
+                    if(!hit){
+                        issue.original.meta.push({ key: key, val: val });
+                    }
+                }
+            }
+        });
     }
 
     // without args or with boolean, returns raw attr hash from first issue or array of hashes from all issues (includes title/created/creator)
     // with key specified, returns matching value from first issue
     // with key/val, or key/val hash specified, updates all issues with key/val attrs
-    function attr(key, val){
-        // creates or overwrites existing meta item with new key/val
-        var updateMeta = function(obj){
-            this.each(function(issue){
-                for(key in obj){
-                    var val = obj[key];
-                    if(key === "title" || key === "created" || key === "creator" || key === "body"){
-                        issue.original[key] = val;
-                    } else {
-                        var hit = false;
-                        // check all original meta values
-                        utils.each(issue.original.meta, function(meta){
-                            if(meta.key === key){
-                                meta.val = val;
-                                hit = true;
-                            }
-                        });
-                        if(!hit){
-                            issue.original.meta.push({ key: key, val: val });
-                        }
-                    }
-                }
-            })
-        };
+    function attr(collection, key, val){
+        // debugger
         // if we have a key and val, update issue meta
         if(typeof key === "string" && typeof val === "string"){
             var obj = {};
             obj[key] = val;
-            updateMeta.call(this, obj);
-            return this;
+            updateMeta(collection, obj);
+            return collection;
         // if object passed in, update issue meta with object
         } else if(utils.isObject(key)){
-            updateMeta.call(this, key);
-            return this;
+            updateMeta(collection, key);
+            return collection;
         // if a key string is passed in, get related value from first issue
         } else if(typeof key === "string"){
             if(val){
                 // return array of values for specified key from all issues
                 var arr = [];
-                utils.each(this.attr(val), function(issue){
+                utils.each(collection.attr(val), function(issue){
                     arr.push(issue[key]);
                 });
                 return arr;
             } else {
                 // return value for specified key from first issue
-                return this.attr()[key];
+                return collection.attr()[key];
             }
-        } else if (arguments.length === 0 || typeof key === "boolean") {
+        } else if ((typeof key === 'undefined' && typeof val === 'undefined') || typeof key === "boolean") {
             // returns hash of attrs of first issue or array of hashes for all issues if boolean is true
             var arr = [];
-            var how_many = key ? this.length : 1;
+            var how_many = key ? collection.length : 1;
             for(var i = 0; i < how_many; i++){
                 var out = {
-                    title: this[i].original.title,
-                    created: this[i].original.created,
-                    creator: this[i].original.creator,
-                    // TODO: should this return body?
-                    body: this[i].original.body
+                    title: collection[i].original.title,
+                    created: collection[i].original.created,
+                    creator: collection[i].original.creator,
+                    // TODO: should we return body here?
+                    body: collection[i].original.body
                 };
-                utils.each(this[i].original.meta, function(meta){
+                utils.each(collection[i].original.meta, function(meta){
                     out[meta.key] = meta.val;
                 });
-                utils.each(this[i].updates, function(update){
+                utils.each(collection[i].updates, function(update){
                     utils.each(update.meta, function(meta){
                         out[meta.key] = meta.val;
                     });
@@ -190,18 +191,18 @@
 
     // TODO: see how much of this code can be merged with attr and proxied through there
     // same as `.attr` but only for meta (i.e. without title/created/creator/body)
-    function meta(key, val){
+    function meta(collection, key, val){
         if(utils.isObject(key) || (typeof key === "string" && typeof val === "string")){
-            return this.attr(key, val);
+            return attr(collection, key, val);
         } else if(typeof key === "string"){
             // TODO: move get meta from `.attr` method to here, let `.attr` call this, and augment it
-            return this.attr(key, val);
+            return attr(collection, key, val);
         } else if (arguments.length === 0 || typeof key === "boolean"){
             var arr = [];
-            var how_many = key ? this.length : 1;
+            var how_many = key ? collection.length : 1;
             for(var i = 0; i < how_many; i++){
                 var out = {};
-                utils.each(this[i].meta, function(meta){
+                utils.each(collection[i].meta, function(meta){
                     out[meta.key] = meta.val;
                 });
                 arr.push(out);
@@ -210,13 +211,13 @@
         }
     }
 
-    function comments(){
-        if(this.length > 1){
+    function comments(collection){
+        if(collection.length > 1){
             utils.debug.warn('`issuemd.fn.comments` is meant to operate on single issue, but got more than one, so will ignore others after first.');
         }
-        if(this[0]){
+        if(collection[0]){
             var out = [];
-            utils.each(this[0].updates, function(update){
+            utils.each(collection[0].updates, function(update){
                 if(utils.typeof(update.body) === 'string' && update.body.length){
                     out.push(utils.copy(update));
                 }
@@ -230,28 +231,27 @@
     // TODO: fix this - logic is all wrong - needs to accept any issue-ish things, and return existing issue with intput merged/concatentated into collection
     // TODO: should this function validate conflicts on original object?
     // merges one or more issues from issuePOJO or issueMD into issues
-    function localmerge(input){
+    function localmerge(collection, input){
 
         var arr = issue_array_from_anything(input);
 
-        var hashes = this.attr("hash", true);
+        var hashes = collection.attr("hash", true);
 
-        var that = this;
         utils.each(arr, function(issue){
             var merged = false;
             for(var i = 0; i < issue.original.meta.length; i++){
                 var idx = utils.indexOf(hashes, issue.original.meta[i].val);
                 if(issue.original.meta[i].key === "hash" && idx != -1){
-                    merge(that[idx], issue);
+                    merger(collection[idx], issue);
                     merged = true;
                 }
             }
             if (!merged){
-                that.push(issue);
+                collection.push(issue);
             }
         });
 
-        return this;
+        return collection;
 
     }
 
@@ -275,7 +275,7 @@
             return parser.parse(input);
         }
     }
-
+  
     var root = this;
 
     // main entry point for issuemd api
@@ -287,7 +287,7 @@
 
     // get the core components in place
     issuemd.parser = parser;
-    issuemd.merge = merge;
+    issuemd.merge = merger;
     issuemd.utils = utils;
     issuemd.formatter = formatter;
 
@@ -312,23 +312,30 @@
         // if a number is passed, return collection of that many blank issues
         // useful for building issues using chained api
         if(typeof input === "number"){
-            makeIssues.call(this, input);
+            makeIssues(this, input);
             return this;
         }
 
         // assume input is issue like, and try to return it as a collection
         try {
-            var arr = issue_array_from_anything.call(this, input);
+            var arr = issue_array_from_anything(input);
             // TODO: use merge here, instead of blindly pushing new issues
             for(var i=0; i<arr.length; i++){
-                localmerge.call(this, arr[i]);
+                localmerge(this, arr[i]);
             }
         } catch(e) {
-            console.log(e.message)
+            console.log(e.message);
         }
 
         return this;
 
+    };
+
+    passThis = function(fn){
+        return function(){
+            [].unshift.call(arguments, this);
+            return fn.apply(this, arguments);            
+        };
     };
 
     // API Methods
@@ -340,28 +347,26 @@
 
         // enable collections to behave like an Array
         length: 0,
-        toArray: function() {
-            return [].slice.call( this );
-        },
+        toArray: function() { return [].slice.call( this ); },
         push: [].push,
         sort: [].sort,
         splice: [].splice,
         pop: [].pop,
 
         // expose useful functions publicly
-        toJSON: toJSON,
-        toString: toString,
-        md: md,
-        html: html,
-        clone: clone,
-        eq: eq,
-        each: each,
-        remove: remove,
-        update: update,
-        merge: merge,
-        attr: attr,
-        meta: meta,
-        comments: comments
+        toJSON: passThis(toJSON),
+        toString: passThis(toString),
+        md: passThis(md),
+        html: passThis(html),
+        clone: passThis(clone),
+        eq: passThis(eq),
+        each: passThis(each),
+        remove: passThis(remove),
+        update: passThis(update),
+        merge: passThis(localmerge),
+        attr: passThis(attr),
+        meta: passThis(meta),
+        comments: passThis(comments)
     };
 
     // hook module into AMD/require etc... or create as global variable
