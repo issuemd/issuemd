2e9d029e05d4401c9b1bb91b230bf23d71bc26ff
Billy Moon <billy@itaccess.org>
2015-12-13 12:19:56 +0000

    fix sortUpdates sort function

--- src/issuemd-core.js
+++ src/issuemd-core.js
@@ -432,8 +432,8 @@
     issuemd.fn.sortUpdates = function(){
         this.each(function(issue){
 
-            issue[0].updates = issue[0].updates.sort(function (a, b) {
-                return a.modified > b.modified;
+            issue[0].updates.sort(function (a, b) {
+                return (new Date(a.modified)) - (new Date(b.modified));
             });
 
         });
