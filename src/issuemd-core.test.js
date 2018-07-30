7d67610a7a1fbc2c4d8f434810d01bfcc5598839
billy moon <billy.moon@wipro.com>
2018-07-30 15:20:09 +0100

    add some tests for array like syntax

--- src/issuemd-core.test.js
+++ src/issuemd-core.test.js
@@ -46,3 +46,69 @@ describe('issuemd create', () => {
     assert.equal(issueString, threeIssues)
   })
 })
+
+describe('array like syntax', () => {
+  let issues
+
+  beforeEach(() => {
+    issues = issuemd({}, {}, {}).attr({
+      creator: 'someguy',
+      created: fixtures.timeString
+    })
+  })
+
+  it('should look like array of three objects', () => {
+    assert.equal(typeof issues[0], 'object')
+    assert.equal(typeof issues[1], 'object')
+    assert.equal(typeof issues[2], 'object')
+    assert.equal(typeof issues[3], 'undefined')
+    assert.equal(issues.length, 3)
+  })
+
+  it('should have push and pop methods', () => {
+    assert.equal(typeof issues[2], 'object')
+    assert.equal(typeof issues[3], 'undefined')
+    assert.equal(issues.length, 3)
+
+    const lastIssue = issues.pop()
+    assert.equal(typeof issues[2], 'undefined')
+    assert.equal(issues.length, 2)
+
+    issues.push(lastIssue)
+    assert.equal(typeof issues[2], 'object')
+    assert.equal(typeof issues[3], 'undefined')
+    assert.equal(issues.length, 3)
+  })
+
+  it('should format correctly after issues are popped off', () => {
+    issues.pop()
+    issues.pop()
+    assert.equal(issues.toString(), fixtures.emptyIssue)
+  })
+
+  it('should format newly initialised issue taken from array access method', () => {
+    const issueString = issuemd(issues[1]).toString()
+    assert.equal(issueString, fixtures.emptyIssue)
+  })
+
+  it('should have shift and unshift methods', () => {
+    assert.equal(typeof issues[2], 'object')
+    assert.equal(typeof issues[3], 'undefined')
+    assert.equal(issues.length, 3)
+
+    const lastIssue = issues.shift()
+    assert.equal(typeof issues[2], 'undefined')
+    assert.equal(issues.length, 2)
+
+    issues.unshift(lastIssue)
+    assert.equal(typeof issues[2], 'object')
+    assert.equal(typeof issues[3], 'undefined')
+    assert.equal(issues.length, 3)
+  })
+
+  it('should format correctly after issues are shifted off', () => {
+    issues.shift()
+    issues.shift()
+    assert.equal(issues.toString(), fixtures.emptyIssue)
+  })
+})
