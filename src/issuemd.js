04324c32e9a5331aa5638c1f6b27500bb9a0b2db
Luka <siric.luka@gmail.com>
2018-08-02 22:12:43 +0200

    removed smart platform detection

--- src/issuemd.js
+++ src/issuemd.js
@@ -1,42 +1,23 @@
-/* global define */
+const utils = require('./utils')
+const issuemd = require('./core')
+const methods = require('./methods')
+const formatter = require('./formatter')
+const _issuemd = this ? this.issuemd : undefined
 
-void (function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
-    define([], function() {
-      return (root.returnExportsGlobal = factory(root))
-    })
-  } else if (typeof exports === 'object') {
-    // Node. Does not work with strict CommonJS, but
-    // only CommonJS-like enviroments that support module.exports,
-    // like Node.
-    module.exports = factory(root)
-  } else {
-    // Browser globals
-    root.returnExportsGlobal = factory(root)
+// attach methods to Issuemd prototype (`issuemd.fn`)
+utils.each(utils.extend(methods, formatter), function(method, name) {
+  issuemd.fn[name] = function(...args) {
+    return method(this, ...args)
   }
-})(typeof window !== 'undefined' ? window : this, function(root) {
-  const utils = require('./utils')
-  const issuemd = require('./core')
-  const methods = require('./methods')
-  const formatter = require('./formatter')
-  const _issuemd = this ? this.issuemd : undefined
-
-  // attach methods to Issuemd prototype (`issuemd.fn`)
-  utils.each(utils.extend(methods, formatter), function(method, name) {
-    issuemd.fn[name] = function(...args) {
-      return method(this, ...args)
-    }
-  })
-
-  issuemd.noConflict = function() {
-    if (root && root.issuemd === issuemd) {
-      root.issuemd = _issuemd
-    }
-    return this
-  }
-
-  issuemd.utils = utils
-
-  return issuemd
 })
+
+issuemd.noConflict = function() {
+  if (this && this.issuemd === issuemd) {
+    this.issuemd = _issuemd
+  }
+  return this
+}
+
+issuemd.utils = utils
+
+module.exports = issuemd
